5b8b57206f2c550759bd1a6245eff2c8a6e9ba03
Phobetor <andreas.kintzinger@googlemail.com>
2014-03-02 16:18:32 +0100

    fixed delete methods' return type

--- src/Phobetor/Billomat/Client/BillomatClient.php
+++ src/Phobetor/Billomat/Client/BillomatClient.php
@@ -15,7 +15,7 @@ use Phobetor\Billomat\Client\Listener\ErrorHandlerListener;
  * @method array getClientMyself(array $args = array()) {@command Billomat GetClientMyself}
  * @method array createClient(array $args = array()) {@command Billomat CreateClient}
  * @method array updateClient(array $args = array()) {@command Billomat UpdateClient}
- * @method array deleteClient(array $args = array()) {@command Billomat DeleteClient}
+ * @method void deleteClient(array $args = array()) {@command Billomat DeleteClient}
  *
  * CLIENT PROPERTY VALUE RELATED METHODS:
  * @method array getClientPropertyValues(array $args = array()) {@command Billomat GetClientPropertyValues}
@@ -27,7 +27,7 @@ use Phobetor\Billomat\Client\Listener\ErrorHandlerListener;
  * @method array getArticle(array $args = array()) {@command Billomat GetArticle}
  * @method array createArticle(array $args = array()) {@command Billomat CreateArticle}
  * @method array updateArticle(array $args = array()) {@command Billomat UpdateArticle}
- * @method array deleteArticle(array $args = array()) {@command Billomat DeleteArticle}
+ * @method void deleteArticle(array $args = array()) {@command Billomat DeleteArticle}
  *
  * INVOICE RELATED METHODS:
  * @method array getInvoices(array $args = array()) {@command Billomat GetInvoices}
@@ -40,14 +40,14 @@ use Phobetor\Billomat\Client\Listener\ErrorHandlerListener;
  * @method array sendInvoiceEmail(array $args = array()) {@command Billomat SendInvoiceEmail}
  * @method array cancelInvoice(array $args = array()) {@command Billomat CancelInvoice}
  * @method array undoCancelInvoice(array $args = array()) {@command Billomat UndoCancelInvoice}
- * @method array deleteInvoice(array $args = array()) {@command Billomat DeleteInvoice}
+ * @method void deleteInvoice(array $args = array()) {@command Billomat DeleteInvoice}
  *
  * INVOICE ITEM RELATED METHODS:
  * @method array getInvoiceItems(array $args = array()) {@command Billomat GetInvoiceItems}
  * @method array getInvoiceItem(array $args = array()) {@command Billomat GetInvoiceItem}
  * @method array createInvoiceItem(array $args = array()) {@command Billomat CreateInvoiceItem}
  * @method array updateInvoiceItem(array $args = array()) {@command Billomat UpdateInvoiceItem}
- * @method array deleteInvoiceItem(array $args = array()) {@command Billomat DeleteInvoiceItem}
+ * @method void deleteInvoiceItem(array $args = array()) {@command Billomat DeleteInvoiceItem}
  *
  * TEMPLATE RELATED METHODS:
  * @method array getTemplates(array $args = array()) {@command Billomat GetTemplate}
@@ -55,49 +55,49 @@ use Phobetor\Billomat\Client\Listener\ErrorHandlerListener;
  * @method \Guzzle\Http\Message\Response getTemplatePreview(array $args = array()) {@command Billomat GetTemplatePreview}
  * @method array createTemplate(array $args = array()) {@command Billomat CreateTemplate}
  * @method array updateTemplate(array $args = array()) {@command Billomat UpdateTemplate}
- * @method array deleteTemplate(array $args = array()) {@command Billomat DeleteTemplate}
+ * @method void deleteTemplate(array $args = array()) {@command Billomat DeleteTemplate}
  *
  * ARTICLE PROPERTY RELATED METHODS:
  * @method array getArticleProperties(array $args = array()) {@command Billomat GetArticleProperties}
  * @method array getArticleProperty(array $args = array()) {@command Billomat GetArticleProperty}
  * @method array createArticleProperty(array $args = array()) {@command Billomat CreateArticleProperty}
  * @method array updateArticleProperty(array $args = array()) {@command Billomat UpdateArticleProperty}
- * @method array deleteArticleProperty(array $args = array()) {@command Billomat DeleteArticleProperty}
+ * @method void deleteArticleProperty(array $args = array()) {@command Billomat DeleteArticleProperty}
  *
  * CLIENT PROPERTY RELATED METHODS:
  * @method array getClientProperties(array $args = array()) {@command Billomat GetClientProperties}
  * @method array getClientProperty(array $args = array()) {@command Billomat GetClientProperty}
  * @method array createClientProperty(array $args = array()) {@command Billomat CreateClientProperty}
  * @method array updateClientProperty(array $args = array()) {@command Billomat UpdateClientProperty}
- * @method array deleteClientProperty(array $args = array()) {@command Billomat DeleteClientProperty}
+ * @method void deleteClientProperty(array $args = array()) {@command Billomat DeleteClientProperty}
  *
  * USER PROPERTY RELATED METHODS:
  * @method array getUserProperties(array $args = array()) {@command Billomat GetUserProperties}
  * @method array getUserProperty(array $args = array()) {@command Billomat GetUserProperty}
  * @method array createUserProperty(array $args = array()) {@command Billomat CreateUserProperty}
  * @method array updateUserProperty(array $args = array()) {@command Billomat UpdateUserProperty}
- * @method array deleteUserProperty(array $args = array()) {@command Billomat DeleteUserProperty}
+ * @method void deleteUserProperty(array $args = array()) {@command Billomat DeleteUserProperty}
  *
  * TAX RELATED METHODS:
  * @method array getTaxes(array $args = array()) {@command Billomat GetTaxes}
  * @method array getTax(array $args = array()) {@command Billomat GetTax}
  * @method array createTax(array $args = array()) {@command Billomat CreateTax}
  * @method array updateTax(array $args = array()) {@command Billomat UpdateTax}
- * @method array deleteTax(array $args = array()) {@command Billomat DeleteTax}
+ * @method void deleteTax(array $args = array()) {@command Billomat DeleteTax}
  *
  * COUNTRY TAX RELATED METHODS:
  * @method array getCountryTaxes(array $args = array()) {@command Billomat GetCountryTaxes}
  * @method array getCountryTax(array $args = array()) {@command Billomat GetCountryTax}
  * @method array createCountryTax(array $args = array()) {@command Billomat CreateCountryTax}
  * @method array updateCountryTax(array $args = array()) {@command Billomat UpdateCountryTax}
- * @method array deleteCountryTax(array $args = array()) {@command Billomat DeleteCountryTax}
+ * @method void deleteCountryTax(array $args = array()) {@command Billomat DeleteCountryTax}
  *
  * REMINDER TEXT RELATED METHODS:
  * @method array getReminderTexts(array $args = array()) {@command Billomat GetReminderTexts}
  * @method array getReminderText(array $args = array()) {@command Billomat GetReminderText}
  * @method array createReminderText(array $args = array()) {@command Billomat CreateReminderText}
  * @method array updateReminderText(array $args = array()) {@command Billomat UpdateReminderText}
- * @method array deleteReminderText(array $args = array()) {@command Billomat DeleteReminderText}
+ * @method void deleteReminderText(array $args = array()) {@command Billomat DeleteReminderText}
  *
  * @licence MIT
  */
